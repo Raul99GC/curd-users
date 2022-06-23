a0a35de1904aa59728d790c190b23f7810d9fee2
Raul C <raul.gines1999@gmail.com>
2022-06-23 18:55:13 -0500

    arreglando error en useUpdate .catch

--- src/hooks/useUpdate.js
+++ src/hooks/useUpdate.js
@@ -3,18 +3,14 @@ import React from 'react'
 const URL = 'https://users-crud1.herokuapp.com/users/'
 
 const useUpdate = ( id, data, getAllUsers, click) => {
-    console.log('en el axios')
-    console.log(id)
     axios.patch(`${URL}${id}/`, data)
         .then(res => {
-
-            console.log('si se logro')
             console.log(res.data)
             getAllUsers()
             click()
 
         })
-        .catch(err = console.log(err))
+        .catch(err => console.log(err))
 }
 
 export default useUpdate
