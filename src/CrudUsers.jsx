9a33ed2f7520e3a318d6a9b9bb9de094132f5c2c
Raul99GC <raulc99g@gmail.com>
2023-03-25 15:39:23 -0600

    adding a / in the url's end

--- src/CrudUsers.jsx
+++ src/CrudUsers.jsx
@@ -5,70 +5,64 @@ import BtnForm from './components/BtnForm'
 import CardUser from './components/CardUser'
 import useUsers from './hooks/useUsers'
 
+const URL = 'https://users-crud.academlo.tech/users/'
 
 
-URL = 'https://users-crud.academlo.tech/users'
 const CrudUsers = () => {
-   const { handleSubmit, register, reset } = useForm()
-   const [objUpdate, setObjUpdate] = useState()
-   const [clickBtnUser, setClicBtnUser] = useState(false)
+
+  const { handleSubmit, register, reset } = useForm()
+  const [objUpdate, setObjUpdate] = useState()
+  const [clickBtnUser, setClicBtnUser] = useState(false)
+  const users = useUsers(URL)
 
 
-   const click = () => {
-      if (!clickBtnUser) {
-         setClicBtnUser(true)
-      } else {
-         setClicBtnUser(false)
-         const obj = {
-            // email: '',
-            // password: '',
-            // first_name: '',
-            // last_name: '',
-            // birthday: ''
-         }
-         reset(obj)
+  const click = () => {
+    if (!clickBtnUser) {
+      setClicBtnUser(true)
+    } else {
+      setClicBtnUser(false)
+      const obj = {}
+      reset(obj)
 
 
-      }
-   }
+    }
+  }
 
 
-   const users = useUsers()
+  return (
+    <div className='CrudUsers flex'>
 
-   return (
-      <div className='CrudUsers flex'>
+      <BtnForm
+        URL={URL}
+        getAllUsers={users.getAllUsers}
+        handleSubmit={handleSubmit}
+        register={register}
+        reset={reset}
+        objUpdate={objUpdate}
+        setObjUpdate={setObjUpdate}
+        clickBtnUser={clickBtnUser}
+        click={click}
+        setClicBtnUser={setClicBtnUser}
+      />
 
-         <BtnForm
-            URL={URL}
-            getAllUsers={users.getAllUsers}
-            handleSubmit={handleSubmit}
-            register={register}
-            reset={reset}
-            objUpdate={objUpdate}
-            setObjUpdate={setObjUpdate}
-            clickBtnUser={clickBtnUser}
-            click={click}
-            setClicBtnUser={setClicBtnUser}
-         />
-
-         <div className="all-cards flex">
-            {
-               users.users?.map(user => (
-                  <CardUser
-                     URL={URL}
-                     user={user}
-                     key={user.id}
-                     getAllUsers={users.getAllUsers}
-                     click={click}
-                     reset={reset}
-                     setObjUpdate={setObjUpdate}
-                  />
-               ))
-            }
-         </div>
-
+      <div className="all-cards flex">
+        {
+          users.users?.map(user => (
+            <CardUser
+              URL={URL}
+              user={user}
+              key={user.id}
+              getAllUsers={users.getAllUsers}
+              click={click}
+              reset={reset}
+              setObjUpdate={setObjUpdate}
+            />
+          ))
+        }
       </div>
-   )
+
+    </div>
+  )
 }
 
 export default CrudUsers
