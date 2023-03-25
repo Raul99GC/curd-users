fe169bc1dc9aa1494ff8212d92711d3907323f6d
Raul99GC <raulc99g@gmail.com>
2023-03-25 15:50:26 -0600

    aI had changed the structure then-catch for a try-catch

--- src/hooks/useUsers.js
+++ src/hooks/useUsers.js
@@ -1,19 +1,25 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
-const useUsers = () => {
-    const [users, setUsers] = useState()
-    const getAllUsers = () => {
-        axios.get(URL)
-            .then(res => setUsers(res.data))
-            .catch(err => console.log(err))
+const useUsers = (URL) => {
+    const [users, setUsers] = useState([])
+
+    const getAllUsers = async () => {
+        try {
+            const res = await axios.get(URL)
+            setUsers(res.data)
+
+        } catch (error) {
+            console.log(error)
+        }
+
     }
 
     useEffect(() => {
         getAllUsers()
     }, [])
 
-    return {users, getAllUsers, setUsers}
+    return { users, getAllUsers, setUsers }
 }
 
 export default useUsers
