88d38ebbba081176bc6a766cd9c71a31241ba609
Raul99GC <raulc99g@gmail.com>
2023-03-25 15:35:25 -0600

    fixing the error in the main div and changing the a for a button

--- src/components/BtnForm.jsx
+++ src/components/BtnForm.jsx
@@ -7,28 +7,28 @@ import Form from './Form'
 const BtnForm = ({ getAllUsers, handleSubmit, register, reset, objUpdate, setObjUpdate, clickBtnUser, click, showForm }) => {
 
 
-    return (
+  return (
 
-        <div div className="icon-form flex" >
-            <a  onClick={click}>
-                <img className='icon-form__img' src={newUserIcon} alt="new user" />
-            </a>
-            {
-                clickBtnUser &&
-                <Form
-                    getAllUsers={getAllUsers}
-                    handleSubmit={handleSubmit}
-                    register={register}
-                    reset={reset}
-                    objUpdate={objUpdate}
-                    setObjUpdate={setObjUpdate}
-                    click={click}
+    <div className="icon-form flex" >
+      <button onClick={click}>
+        <img className='icon-form__img' src={newUserIcon} alt="new user" />
+      </button>
+      {
+        clickBtnUser &&
+        <Form
+          getAllUsers={getAllUsers}
+          handleSubmit={handleSubmit}
+          register={register}
+          reset={reset}
+          objUpdate={objUpdate}
+          setObjUpdate={setObjUpdate}
+          click={click}
 
-                />
-            }
-        </div>
+        />
+      }
+    </div>
 
-    )
+  )
 }
 
 export default BtnForm
